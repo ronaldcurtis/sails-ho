dd41351389960a9c21ca0c857ef738190f1ef0d9
Ronald Curtis <curtis.ronald@abc.net.au>
2014-05-29 12:18:20 +1000

    Update sails to 0.10.0-rc7

--- assets/js/loader.js
+++
@@ -1,3 +0,0 @@
-//= require ./sails/socket.io
-//= require ./sails/sails.io.js
-//= require ./sails/app.js
--- config/session.js
+++ config/session.js
@@ -1,21 +1,31 @@
 /**
- * Session
- * 
- * Sails session integration leans heavily on the great work already done by Express, but also unifies 
- * Socket.io with the Connect session store. It uses Connect's cookie parser to normalize configuration
- * differences between Express and Socket.io and hooks into Sails' middleware interpreter to allow you
- * to access and auto-save to `req.session` with Socket.io the same way you would with Express.
+ * Session Configuration
+ * (sails.config.session)
+ *
+ * Sails session integration leans heavily on the great work already done by
+ * Express, but also unifies Socket.io with the Connect session store. It uses
+ * Connect's cookie parser to normalize configuration differences between Express
+ * and Socket.io and hooks into Sails' middleware interpreter to allow you to access
+ * and auto-save to `req.session` with Socket.io the same way you would with Express.
  *
  * For more information on configuring the session, check out:
- * http://sailsjs.org/#documentation
+ * http://links.sailsjs.org/docs/config/session
  */
 
 module.exports.session = {
 
   // Session secret is automatically generated when your new app is created
   // Replace at your own risk in production-- you will invalidate the cookies of your users,
-  // forcing them to log in again. 
-  secret: '99dd4a7ad4ebb7b6b7da76ca2f66766f',
+  // forcing them to log in again.
+  secret: 'b3480fad1fdc24549fa70cbbd2c9dc26',
+
+
+  // Set the session cookie expire time
+  // The maxAge is set by milliseconds, the example below is for 24 hours
+  //
+  // cookie: {
+  //   maxAge: 24 * 60 * 60 * 1000
+  // }
 
 
   // In production, uncomment the following lines to set up a shared redis session store
@@ -35,17 +45,22 @@ module.exports.session = {
 
 
   // Uncomment the following lines to use your Mongo adapter as a session store
-  adapter: 'mongo',
-  
-  host: 'localhost',
-  port: '27017',
-  db: 'sails-ho',
-  collection: 'sessions',
-  
-  username: '',
-  password: '',
-  auto_reconnect: false,
-  ssl: false,
-  stringify: true
+  // adapter: 'mongo',
+  //
+  // host: 'localhost',
+  // port: 27017,
+  // db: 'sails',
+  // collection: 'sessions',
+  //
+  // Optional Values:
+  //
+  // # Note: url will override other connection settings
+  // url: 'mongodb://user:pass@host:port/database/collection',
+  //
+  // username: '',
+  // password: '',
+  // auto_reconnect: false,
+  // ssl: false,
+  // stringify: true
 
 };
